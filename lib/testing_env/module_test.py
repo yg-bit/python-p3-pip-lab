3cf274cc74837bda66f4b7e2ae447220ad2fe15f
yg-bit <jamesisaiah164@gmail.com>
2025-12-05 14:44:18 +0300

    added

--- lib/testing_env/module_test.py
+++ lib/testing_env/module_test.py
@@ -7,7 +7,7 @@ from versions import (
 def test_python_version():
     version_info = python_version()
     assert version_info.major == 3
-    assert version_info.minor == 8
+    assert version_info.minor == 13
 
 
 def test_requests_version():
